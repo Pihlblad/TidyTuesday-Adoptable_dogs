6b6924b23fd5a9b134e487ec1f558c32c24fafff
Johanna Pihlblad <johanna.pihlblad@gmail.com>
2019-12-19 16:35:24 +1100

    group by breed, age, sex

--- run.R
+++ run.R
@@ -5,3 +5,23 @@
 dog_moves <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_moves.csv')
 dog_travel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_travel.csv')
 dog_descriptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_descriptions.csv')
+
+library(tidyverse)
+
+# Survay
+
+summary(dog_moves)
+summary(dog_travel)
+summary(dog_descriptions)
+
+# Clean 
+
+df <- dog_descriptions %>% 
+  mutate_if(is.character, as.factor) 
+
+df %>% 
+  select(breed_primary, age, sex) %>% 
+  group_by(breed_primary, age, sex) %>%
+  summarise(count=n())%>% 
+  arrange(desc(count))
+  
